b339dd425001138dd08a38184b9dde32e184a64c
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-12-26 02:35:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function findMatching(drivers, string) {
   return drivers.filter(function(driver){
     return driver.toLowerCase() === string.toLowerCase()
   });
-} 
+}
 
 function fuzzyMatch(drivers, string) {
   return drivers.filter(function(driver) {
